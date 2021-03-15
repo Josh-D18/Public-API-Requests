49eb68babcb680a2fa8f1cfd5c578de0f740db71
unknown <jjdate@live.com>
2021-03-15 02:36:45 -0400

    working on adding modal

--- js/index.js
+++ js/index.js
@@ -1,27 +1,17 @@
-// ---- Helper Functions -----
+// HTML elements
+const gallery = document.querySelector('#gallery');
+const searchContainer = document.querySelector('.search-container');
+const modal = document.createElement('div');
+modal.setAttribute('class', 'modal-container');
 
-// Fetching the API
-function fetchData(url){
-    return fetch(url)
-    .then(res => res.json())
-}
 
-// Function to call other functions
-function callAPI(url){
-    addImage(url);
-    addName(url);
-    addEmail(url);
-    addCityAndCountry(url);
-}
 
+// Inserting HTML Into DOM
 function addHTML(htmlElement, htmlToAdd){
     htmlElement.insertAdjacentHTML('afterbegin', htmlToAdd)
 }
 
-
-
 // Form Markup
-const searchContainer = document.querySelector('.search-container');
 const searchForm = `
 <form action="#" method="get">
     <input type="search" id="search-input" class="search-input" placeholder="Search...">
@@ -31,154 +21,101 @@ const searchForm = `
 
 
 
-// Gallery Markup
-const gallery = document.querySelector('#gallery');
-const galleryHTML = `
-<div class="card">
-    <div class="card-img-container">
-        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-    </div>
-    <div class="card-info-container">
-        <h3 id="name" class="card-name cap">first last</h3>
-        <p class="card-text">email</p>
-        <p class="card-text cap">city, state</p>
-    </div>
-</div>`;
+// Fetching the API
+
+// Gallery View
+fetch('https://randomuser.me/api/?results=12')
+    .then(res => res.json())
+    .then(data => getUsers(data))
+    .catch(err => new Error(`Something went Wrong: ${err}`, err));
+
+// Modal View
+fetch('https://randomuser.me/api/?results=12')
+    .then(res => res.json())
+    .then(data => addmodalHTML(data))
+
+//Gallery Markup
+
+function getUsers(data) { 
+    const html = data.results.map(person =>  
+        `<div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${person.picture.thumbnail}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="card-text">${person.email}</p>
+                <p class="card-text cap">${person.location.city} ${person.location.country}</p>
+            </div>
+        </div>`
+    ).join('');
+    gallery.insertAdjacentHTML('afterbegin', html);
+}
 
 
 // Modal Markup
-const modal = document.createElement('div');
-modal.setAttribute('class', 'modal-container');
 
-const modalHTML = ` 
-<div class="modal">
-    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-<div class="modal-info-container">
-    <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-    <h3 id="name" class="modal-name cap">name</h3>
-    <p class="modal-text">email</p>
-    <p class="modal-text cap">city</p>
-    <hr>
-    <p class="modal-text">(555) 555-5555</p>
-    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-    <p class="modal-text">Birthday: 10/21/2015</p>
-</div>`;
+function addmodalHTML(data){
+    const cards = document.querySelectorAll('.card');
+    const html = data.results.map(person =>
+    `
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+        <img class="modal-img" src="${person.picture.thumbnail}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+        <p class="modal-text">${person.email}</p>
+        <p class="modal-text cap">${person.location.city} ${person.location.country}</p>
+        <hr>
+        <p class="modal-text">${person.cell}</p>
+        <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.country} ${person.location.state}, ${person.location.postcode}</p>
+        <p class="modal-text">Birthday: ${person.dob.date}</p>
+    </div>
+    `)
+    console.log(cards)
+    let cardList = [...cards];
 
-
-
-
-
-
-// Inserting HTML Into DOM
-addHTML(searchContainer, searchForm);
-addHTML(gallery, galleryHTML);
-addHTML(modal, modalHTML);
-
-
-
-
-// HTML Elements For Modal
-
-
-// Add Image to API
-function addImage(url){
-    fetchData(url)
-    .then(data => {
-        const galleryImg = document.querySelector('.card-img');
-        data.results.map(person => {
-            const image = person.picture.thumbnail;
-            galleryImg.src = image;
-        });
-    });
-}
-
-
-// Add Name to API
-function addName(url){
-    fetchData(url)
-    .then(data => {
-        const nameElement = document.querySelector('#name');
-        data.results.map(person => {
-            const firstName = person.name.first;
-            const lastName = person.name.last;
-            nameElement.textContent = `${firstName} ${lastName}`;
-        });
-    });
-}
-
-function addEmail(url){
-    fetchData(url)
-    .then(data => {
-        const emailElement = document.querySelector('.card-text');
-        data.results.map(person => {
-            const email = person.email;
-            emailElement.textContent = email;
-        });
-    });
-}
-
-function addCityAndCountry(url){
-    fetchData(url)
-    .then(data => {
-        const cityAndCountry = document.querySelector('.card-text.cap');
-        data.results.map(person => {
-            const city = person.location.city;
-            const country = person.location.country;
-            cityAndCountry.textContent = `${city} ${country}`;
-        });
-    });
-
-}
-
-
-// Fecthing data and changing HTML
-
-// function getUsers(){
-//     // HTML Elements For Modal
-//     const galleryImg = document.querySelector('.card-img');
-//     const nameElement = document.querySelector('#name');
-//     const cityAndCountry = document.querySelector('.card-text.cap');
-//     const emailElement = document.querySelector('.card-text');
-
-//     fetchData('https://randomuser.me/api/?results=6')
-//     .then(data => {
-//         data.results.map(person => {
-//             // Info from API
-//             const image = person.picture.thumbnail;
-//             const firstName = person.name.first;
-//             const lastName = person.name.last;
-//             const city = person.location.city;
-//             const country = person.location.country;
-//             const email = person.email;
+    cardList.map(card => {
+        card.addEventListener('click', () => {
+            card.insertAdjacentHTML("afterend", html);
+            // console.log(document.querySelector('#modal-close-btn'))
+            
         
-//             // Changing HTML 
-//             galleryImg.src = image; 
-//             nameElement.textContent = `${firstName} ${lastName}`;
-//             emailElement.textContent = email;
-//             cityAndCountry.textContent = `${city} ${country}`;
-//             gallery.insertAdjacentHTML('afterbegin', galleryHTML);
-//         });
-//         // const img = `<img class="card-img" src="${image}" alt="profile picture">`;
-//         //     const nameText = `<h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>`;
-//         //     const emailText = `<p class="card-text">${email}</p>`;
-//         //     const cityText = `<p class="card-text cap">${city} ${country}</p>`;
-// });
+            // X button Event
+            // const closeBtn = document.querySelector('#modal-close-btn');
+            // const x = document.querySelector('strong');
+        
+            // closeBtn.addEventListener('click', (e) => {
+            // const modal = document.querySelector('.modal-container');
+            // console.log(e.target)
+            // if (e.target === closeBtn || e.target === x){
+            //     modal.style.display = 'none';
+            //     elementClicked = true;
+            // } else{
+            //     elementClicked = false;
+            //     modal.style.display === ''
+            // }
+            // });
+        });
+    });
+} 
 
-// }
 
 
 
+
+
+
+
+
+//----- Add Modal Info from API to HTML
 
 
 
 // Modal Window
 
-const cards = document.querySelector('.card');
-console.log(cards)
-cards.addEventListener('click', () => {
-    gallery.insertAdjacentElement("afterend", modal);
-});
 
 
-// Send request to the api
-callAPI('https://randomuser.me/api/?results=12');
+
+addHTML(searchContainer, searchForm);
+// addHTML(modal, modalHTML);
